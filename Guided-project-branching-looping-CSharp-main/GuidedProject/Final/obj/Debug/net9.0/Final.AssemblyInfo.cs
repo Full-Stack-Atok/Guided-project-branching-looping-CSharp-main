9182740f8dde8aa3d4d4b61d41630b7780451744
Full-Stack-Atok <komsaykerby@gmail.com>
2025-02-17 10:17:00 +0800

    final

--- Guided-project-branching-looping-CSharp-main/GuidedProject/Final/obj/Debug/net9.0/Final.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/GuidedProject/Final/obj/Debug/net9.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca5c9cda657f6781e6ad241a574f439835cd30d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
